4995f2d3fc5b8e1a8ec17d4dfc0af2055dd9a5e2
Nimrod02 <letrun.a@gmail.com>
2023-02-15 06:17:04 -0800

    change a lot of thing

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,14 +4,15 @@
  *
  * Return: always 0
  */
-int main() 
+int main(void) 
 {
 printf("Size of char: %ld bytes\n", sizeof(char));
 printf("Size of int: %ld bytes\n", sizeof(int));
 printf("Size of long: %ld bytes\n", sizeof(long int));
 printf("Size of long: %ld bytes\n", sizeof(long long int));
-printf("Size of float: %ld bytes\n", sizeof(float));
+printf("Size of float: %ld bytes\n", sizeof(float);
 printf("Size of double: %ld bytes\n", sizeof(double));
 printf("Size of long double: %ld bytes\n", sizeof(long double));
 return 0;
 }
+
