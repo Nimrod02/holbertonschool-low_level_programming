cc006eb6d49f4a9372559e89cb1e049249275992
Nimrod02 <letrun.a@gmail.com>
2023-02-15 04:51:03 -0800

    add new script 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+  
+/**
+ * main - Print "Programming is like building a multilingual puzzle"
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+printf("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
