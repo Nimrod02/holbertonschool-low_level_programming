844f7a58c5ba694036f2a8e769f7e2e2c2262b50
Nimrod02 <letrun.a@gmail.com>
2023-02-15 04:07:11 -0800

    change syntax { }

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle\n");
+int main()
+{
+    puts ("Programming is like building a multilingual puzzle\n);
     return 0;
 }
 
