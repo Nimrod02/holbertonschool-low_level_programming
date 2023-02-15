8f2925caad4b25e4b597c0143cfff220fa3022d7
Nimrod02 <letrun.a@gmail.com>
2023-02-15 04:26:36 -0800

    add comentary and change syntax

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Print "Programming is like building a multilingual puzzle"
+ *
+ * return: always 0
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
 
