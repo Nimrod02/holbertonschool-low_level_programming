61e0c1853fceac31cf9c09e44a4ce66d4e2cbfff
Nimrod02 <letrun.a@gmail.com>
2023-02-15 04:52:26 -0800

    add new script 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
   
 /**
- * main - Print "Programming is like building a multilingual puzzle"
+ * main - Print "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: always 0
  */
 int main(void)
 {
-printf("\"Programming is like building a multilingual puzzle");
+printf("\"with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
 
