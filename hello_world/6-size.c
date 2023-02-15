e50c17a2109576f6d8de0655a5f5fc692ca435b3
Nimrod02 <letrun.a@gmail.com>
2023-02-15 06:19:38 -0800

    change syntax 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,7 +4,7 @@
  *
  * Return: always 0
  */
-int main(void) 
+int main(void)
 {
 printf("Size of char: %ld bytes\n", sizeof(char));
 printf("Size of int: %ld bytes\n", sizeof(int));
@@ -13,6 +13,6 @@ printf("Size of long: %ld bytes\n", sizeof(long long int));
 printf("Size of float: %ld bytes\n", sizeof(float);
 printf("Size of double: %ld bytes\n", sizeof(double));
 printf("Size of long double: %ld bytes\n", sizeof(long double));
-return 0;
+return (0);
 }
 
