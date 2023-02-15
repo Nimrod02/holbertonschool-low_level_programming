bb44ed9c8bb899edf0648bf81613ca36cd8e1bbf
Nimrod02 <letrun.a@gmail.com>
2023-02-15 05:50:02 -0800

    change syntax 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,8 @@ int main(void)
 {
 printf("Size of char: %ld bytes\n", sizeof(char));
 printf("Size of int: %ld bytes\n", sizeof(int));
-printf("Size of long: %ld bytes\n", sizeof(long));
+printf("Size of long int: %ld bytes\n", sizeof(long int));
+printf("Size of long long int: %ld bytes\n", sizeof(long long long int));
 printf("Size of float: %ld bytes\n", sizeof(float));
 printf("Size of double: %ld bytes\n", sizeof(double));
 printf("Size of long double: %ld bytes\n", sizeof(long double));
