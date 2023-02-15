484a1c1907aeceac725d89a50573582bec4cc00a
Nimrod02 <letrun.a@gmail.com>
2023-02-15 06:47:36 -0800

    mac de merde

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,11 +13,11 @@ long int longIntType;
 long long int longLongIntType;
 float floatType;
 
-printf("Size of char: %ld bytes\n", sizeof(char));
-printf("Size of int: %ld bytes\n", sizeof(int));
-printf("Size of long int: %ld bytes\n", sizeof(long int));
-printf("Size of long long int: %ld bytes\n", sizeof(long long int));
-printf("Size of float: %ld bytes\n", sizeof(float);
+printf("Size of char: %zu byte(s)\n", sizeof(char));
+printf("Size of int: %zu byte(s)\n", sizeof(int));
+printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of float: %zu byte(s)\n", sizeof(float);
 return (0);
 }
 
