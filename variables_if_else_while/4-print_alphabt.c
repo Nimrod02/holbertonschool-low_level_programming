d44696d6d04643c1817c7d38171a6a68f1c3e202
Nimrod02 <letrun.a@gmail.com>
2023-02-20 05:39:45 -0800

    add new file 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+		       a++;
+		}
+
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
