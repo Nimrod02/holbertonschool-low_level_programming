0fa4fe43944aa4e6ac160bc607a943716ce9d80e
Nimrod02 <letrun.a@gmail.com>
2023-02-20 05:53:20 -0800

    add new file 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = '0'; 0 <= '9'; n++)
+	{
+		putchar(0);
+	}
+	putchar('\n');
+	return (0);
+}
+
