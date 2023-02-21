4e344edec467e1bf411699829e4846ac97f2cbd9
Nimrod02 <letrun.a@gmail.com>
2023-02-21 01:40:26 -0800

    add file .c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+		char a;
+
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar(a);
+}
