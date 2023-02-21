03db909cb95267e145bc5a96f5029557b49346fc
Nimrod02 <letrun.a@gmail.com>
2023-02-21 03:53:12 -0800

    add new files 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+		char a;
+		int c = 0;
+
+		while (c <= 9)
+		{	
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+		c++;
+		}
+}
