3d80e0c458667bc249c36f8e96351318a8270971
Nimrod02 <letrun.a@gmail.com>
2023-02-21 05:47:45 -0800

    add new files 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
+ * main - isalpha print int c
+ * @c: the character being checked
  *
- *
+ * Return: 1 if lowercase and 0 if else
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' &&  c <= 'z') || ( c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' &&  c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
