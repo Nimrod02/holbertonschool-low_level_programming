89b5ff4331822162ac65376bf2d86604bc69c29b
Nimrod02 <letrun.a@gmail.com>
2023-02-22 05:21:41 -0800

    add new files 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ */
+
+void times_table(void)
+{
+	int col, lig, res;
+	
+		for (lig = 0; lig <= 9; lig++)
+		{
+			for (col = 0; col <= 9; col++) 
+			{
+				res = lig * col;
+
+				if (col == 0)
+				{
+					_putchar('0'+ res);
+				}
+				else if (res <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + res);
+				}
+				else if (res > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (res / 10));
+					_putchar('0' + (res % 10));
+				}
+			}
+			_putchar('\n');
+		}
+}
+
+
+					
+
+
