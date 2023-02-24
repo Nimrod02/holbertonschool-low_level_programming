b5a003a24e31303f6a4b41fe5a481465b190c110
nimrod02 <letrun.a@gmail.com>
2023-02-24 15:17:02 +0100

    add files 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int l, c;
+
+	if (n > 0)
+	{
+		for (l = 1; l <= n; l++)
+		{
+			for (c = 1; c <= n; n++)
+			{
+				_putchar ('#');
+			}
+		_putchar ('\n');
+		}
+	}
+}
