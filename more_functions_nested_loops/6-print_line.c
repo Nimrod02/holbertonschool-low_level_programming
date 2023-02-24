0b4e8dfd773a7dc2f1ff5edc3fa0ca521d986f25
nimrod02 <letrun.a@gmail.com>
2023-02-24 14:42:12 +0100

    change syntax for betty

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
+/**
+ * print_line - prints as much underscore as the value of n
+ * @n: the number of _
+ */
 
 void print_line(int n)
 {
 	int l;
-	
+
 	for (l = 0; l < n; l++)
 	{
 		_putchar ('_');
 	}
 	_putchar ('\n');
 }
-	
 
