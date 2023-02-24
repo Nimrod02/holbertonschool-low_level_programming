16a7515ccf3bb82d40d577debe495d31c0d35807
nimrod02 <letrun.a@gmail.com>
2023-02-24 09:04:09 +0100

    add betty syntax

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,12 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * mul - print the result of a multiplication of two integer
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of the multiplication of a with b
+ */
 
 int mul(int a, int b)
-{	
+{
 	int res;
+
 	scanf("%d %d", &a, &b);
 	res = (a * b);
-	printf("%d", res); 
+	printf("%d", res);
 	return (0);
 }
