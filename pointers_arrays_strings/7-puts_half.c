0143ffecbd3ef5df24119bcb3b20b0314e0bf5d4
nimrod02 <letrun.a@gmail.com>
2023-02-28 16:39:01 +0100

    add files 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strlen - give the lengh of a string
+ * @str: the string
+ *
+ * Return: the lengh of s
+ */
+
+int _strlen(char *str)
+{
+	int length = 0;
+
+		for (; *str != '\0'; str++)
+		{
+			length++;
+		}
+	return (length);
+}
+
+void puts_half(char *str)
+{
+	int a;
+
+	for (a = 5; str[a] != '\0' && a < _strlen(str); a++)
+	{
+		_putchar (str[a]);
+	}
+	_putchar ('\n');
+}
