61e96150517fe6ffd09937e1574d128f14471b4c
nimrod02 <letrun.a@gmail.com>
2023-03-01 13:41:47 +0100

    add files 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+
+/**
+ * _strlen - give the lengh of a string
+ * @s: the string
+ *
+ * Return: the lengh of s
+ */
+
+int _strlen(char *src)
+{
+	int length = 0;
+
+		for (; *src != '\0'; src++)
+		{
+			length++;
+		}
+	return (length);
+}
+
+
+/**
+ * _strcpy - copies the string pointed by src into dest
+ * @dest: destination
+ * @src: source
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+		for (; index <= _strlen(src); index++)
+		{
+			dest[index] = src[index];
+		}
+		return (dest);
+}
