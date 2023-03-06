675685627d1cefdc4342f0a8a7218a4541c1061e
nimrod02 <letrun.a@gmail.com>
2023-03-06 14:01:34 +0100

    change syntax

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,15 +8,10 @@
  */
 
 int _strlen_recursion(char *s)
-
 {
-	int length = 0;
 
-	if (s[length] >= '0' && s[length] != '\0')
-	{
-		length++;
-		return (*s);
-	}
-	_strlen_recursion(s + 1);
-	return (0);
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
 }
