7265a7ab34eb3b51d9ede82924f901cc46ca8963
nimrod02 <letrun.a@gmail.com>
2023-03-07 09:25:09 +0100

    add betty syntax 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * sqrt_helper - find the root of n
+ * @n: n
+ * @root: test this root
+ *
+ * Return: nat square root, else -1
+ */
+
 int sqrt_helper(int n, int root)
 {
 	if (root * root > n)
@@ -8,6 +16,13 @@ int sqrt_helper(int n, int root)
 		return (root);
 	return (sqrt_helper(n, root + 1));
 }
+
+/**
+ * _sqrt_recursion - find natural square root of n
+ * @n: n
+ *
+ * Return: result1
+ */
 
 int _sqrt_recursion(int n)
 {
