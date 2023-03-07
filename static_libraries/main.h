c57712188a126d00d29cb98f5acae06b42ff38bd
nimrod02 <letrun.a@gmail.com>
2023-03-07 15:57:23 +0100

    change all

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,7 +1,7 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
-#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,4 +23,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif /*_main_h_*/
+
+#endif
