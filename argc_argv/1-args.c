ae79ed49c9a801971fde400f4b8cd9d0258ff866
nimrod02 <letrun.a@gmail.com>
2023-03-08 13:09:10 +0100

    add files 1, arg

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * main - print name of program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
