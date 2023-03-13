8544b095ee9746603dcde35e46c92fab76720806
nimrod02 <letrun.a@gmail.com>
2023-03-13 13:45:25 +0100

    add new dir, file and readme

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - create array of chars and initialize with a specific char
+ * @size: size of array
+ * @c: fill array values with this char
+ *
+ * Return: pointer to array
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int index = 0;
+
+	array = malloc(size * (sizeof(char)));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (; index < size; index++)
+	{
+		array[index] = c;
+	}
+
+	return (array);
+	free(array);
+}
