c8c3e3d78277b718c24521b69a198c77b21516e5
nimrod02 <letrun.a@gmail.com>
2023-03-15 10:46:42 +0100

    add file with betty syntax

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+* main - print the current file
+*
+* Return: 0 is a success
+*/
 
 int main(void)
 {
-    printf("%s", __FILE__);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
