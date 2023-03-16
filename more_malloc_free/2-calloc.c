e7c38a1bf780d3005d39babab1933760734251a4
nimrod02 <letrun.a@gmail.com>
2023-03-16 13:50:17 +0100

    add file 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int count = 0;
+
+	if (nmemb <= 0 || size <= 0)
+	return (NULL);
+
+	ptr = malloc(nmemb * size);
+    
+	if (ptr == NULL)
+		return (NULL);
+
+	while (count < nmemb * size)
+    	{
+		ptr[count] = 0;
+		count++;
+	}
+    return (ptr);
+}
