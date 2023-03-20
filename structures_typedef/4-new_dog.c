bc9ca45754bff884929fbbb3637b77ed434faa2f
nimrod02 <letrun.a@gmail.com>
2023-03-20 14:41:55 +0100

    add modified task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,7 +15,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog = malloc(sizeof(dog_t) + 1);
 
 	if (!new_dog)
+	{
+		free (new_dog);
 		return (NULL);
+	}
 
 	new_dog->name = name;
 	new_dog->age = age;
