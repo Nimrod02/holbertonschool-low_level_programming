1d9068b1dfefbe8a74691de5aee5ae5d7feab7a2
nimrod02 <letrun.a@gmail.com>
2023-03-20 14:43:19 +0100

    add modified task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,13 +16,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (!new_dog)
 	{
-		free (new_dog);
+		free(new_dog);
 		return (NULL);
 	}
 
-	new_dog->name = name;
+	new_dog->name = strdup(name);
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = strdup(owner);
 
 return (new_dog);
 }
