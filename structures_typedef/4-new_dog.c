4c550047d863395c9f94a47bd76c4ecdb7a3e9f0
nimrod02 <letrun.a@gmail.com>
2023-03-21 10:51:56 +0100

    add new syntax for the task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,24 +13,33 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	dog_t *puppy;
 
-	if (new_dog == NULL)
+	puppy = malloc(sizeof(dog_t));
+
+	if (puppy == NULL)
+		return (NULL);
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	puppy->name = malloc(sizeof(char) * strlen(name) + 1);
+	puppy->owner = malloc(sizeof(char) * strlen(owner) + 1);
+
+	if (puppy->name == NULL)
 	{
+		free (puppy);
 		return (NULL);
 	}
-	new_dog->name = strdup(name);
-	new_dog->owner = strdup(owner);
-
-	if (new_dog->name == NULL|| new_dog->owner == NULL)
+	if (puppy->owner == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog->owner);
-		free(new_dog);
+		free (puppy->name);
+		free (puppy);
 		return (NULL);
 	}
 
-	new_dog->age = age;
-
-return (new_dog);
+	puppy->name = strcpy(puppy->name, name);
+	puppy->age = age;
+	puppy->owner = strcpy(puppy->owner, owner);
+return (puppy);
 }
