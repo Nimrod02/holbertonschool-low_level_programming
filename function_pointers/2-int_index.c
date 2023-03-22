798f39f151f4863bd8aaaec655be390e03090ed7
nimrod02 <letrun.a@gmail.com>
2023-03-22 09:43:41 +0100

    [FIX] Memory leaks on file 2-int_index.c and betty syntax

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -14,8 +14,8 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index = 0;
-	
-	if (size <= 0)
+
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (; index < size; index++)
