0a93092fbfae92155ed00bbf3e9fde4cc9045ed2
nimrod02 <letrun.a@gmail.com>
2023-03-22 09:41:01 +0100

    [FIX] Memory leaks on file 2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -14,13 +14,17 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index = 0;
-
+	
 	if (size <= 0)
 		return (-1);
 
 	for (; index < size; index++)
+	{
 		if (cmp(array[index]) != 0)
+		{
 			return (index);
+		}
+	}
 
 return (-1);
 }
