e17302f4e54dab72e25c2eb509575c3b12ca816e
nimrod02 <letrun.a@gmail.com>
2023-03-22 14:48:19 +0100

    [ADD] New directory 'Variadic_functions', add 'variadic_functions.h', my personnal header file, and file '0-sum_them_all' file for the task.

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sum_them_all - main funtion that add all the variables
+ * @n: unsigned int that contain all variables
+ *
+ * Return: sum of the operation.
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int index = 0;
+	int sum = 0;
+
+	va_start(ap, n);
+
+	for (; index < n; index++)
+		sum += va_arg(ap, unsigned int);
+	va_end(ap);
+return (sum);
+}
