7c0c8177f6d0fdfcaad9a1851d5460b6702bab89
nimrod02 <letrun.a@gmail.com>
2023-03-22 09:32:34 +0100

    [ADD] file 2-int_index and update file .h for the task 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * int_index - main function, that search the int in the array.
+ * @array: array of the values.
+ * @size: number of elements in the array.
+ * @cmp: compare the element of the array.
+ * Return: -1 if no elements.
+ *
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	for (; index < size; index++)
+		if (cmp(array[index]) != 0)
+			return (index);
+
+return (-1);
+}
