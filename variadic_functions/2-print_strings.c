27ca04542c8e2db33ccd61da86b4a4314a1378ce
nimrod02 <letrun.a@gmail.com>
2023-03-23 15:41:07 +0100

    [ADD] File 2 print_string and update header file with prototype

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - main function that print a string
+ * @separator: separation beetween two string
+ * @n: contain the two string
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strg;
+
+	char *str;
+	unsigned int index = 0;
+
+	va_start(strg, n);
+
+	if (n > 0)
+	for (; index < n; index++)
+	{
+		str = va_arg(strg, char *);
+			if (strg == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
+
+			if (index < n - 1 && separator != NULL)
+				printf("%s", separator);
+	}
+	putchar ('\n');
+}
