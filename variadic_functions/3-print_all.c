248980257742f9fce96674813fbb79f5eec11030
nimrod02 <letrun.a@gmail.com>
2023-03-27 08:49:15 +0200

    [ADD] file for task 3, 3-print_all and update header file with the prototype.

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,94 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <type.h>
+
+/**
+ * print_char - main function that print a character
+ * @form: format
+ *
+*/
+void print_char(va_list form)
+{
+	printf("%c", va_arg(form, char));
+}
+
+/**
+ * print_int - main function that print a integer
+ * @form: format
+ *
+*/
+
+void print_int(va_list form)
+{
+	printf("%d", va_arg(form, int));
+}
+
+/**
+ * print_float - main function that print a float
+ * @form: format
+ *
+*/
+
+void print_float(va_list form)
+{
+	printf("%f", va_arg(form, double));
+}
+
+/**
+ * print_string - main function that print a string
+ * @form: format
+ *
+*/
+
+void print_string(va_list form)
+{
+	char *str;
+
+	str = va_arg(form, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+
+/**
+ * print_all - main function that print all the type asked
+ * @format: array of data type
+ *
+*/
+
+void print_all(const char * const format, ...)
+{
+	char *space = "";
+	int index1 = 0, index2 = 0;
+
+	va_list dt;
+
+	datatype choice[] = { {'c', print_char},
+			      {'i', print_int},
+			      {'f', print_float},
+			      {'s', print_string},
+			      {'\0', NULL} };
+
+	va_start(dt, format);
+	while (format != NULL && format[j] != '\0')
+	{
+		while (choice[index1].letter != '\0')
+		{
+			if (choice[index1].letter == format[j])
+			{
+				printf("%s", separator);
+				choice[index1].func(valist);
+				separator = ", ";
+			}
+			index1++;
+		}
+		index2++;
+	}
+	va_end(valist);
+	printf("\n");
+}
