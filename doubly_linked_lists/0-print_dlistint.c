e002de049c916359319fe30d6d6b52e7b3c0eb60
nimrod02 <letrun.a@gmail.com>
2023-04-05 14:58:59 +0200

    add new dir for the project 2 with a readme the header file and the first file

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - main function that print all the elements
+ * @h: string
+ *
+ * Return: retun the number of nodes use
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t new_nodes = 0;
+
+
+	while (h)
+	{
+		
+		printf("%d\n", h->n);
+		h = h->next;
+		new_nodes++;
+
+	}
+	return (new_nodes);
+}
