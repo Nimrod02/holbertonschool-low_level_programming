966aefe27224513d987387e73eaaea0a8006c9d0
nimrod02 <letrun.a@gmail.com>
2023-04-05 10:03:40 +0200

    update file for task 4 free_list with a new programe

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,16 +1,17 @@
 #include "lists.h"
 
 /**
- * 
- * 
- * 
+ * free_list - main function that free the list of nodes
+ * @head: head is the start of the node list
+ *
 */
 
 void free_list(list_t *head)
 {
-	while (head->next)
+	while (head != NULL)
 	{
-		if (head->next == NULL)
-			free(head);
+		free(head->str);
+		free(head);
+		head = head->next;
 	}
 }
