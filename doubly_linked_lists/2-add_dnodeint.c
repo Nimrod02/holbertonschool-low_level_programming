6ad4f7ff55104847653bcb73686f7825004389db
nimrod02 <letrun.a@gmail.com>
2023-04-06 13:35:38 +0200

    add file for task 2 add_dnodeint and update the header file for the task

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * *add_dnodeint - function that add a node to the start of the of the list
+ * @head: pointer of the start of the list
+ * @n: integer that contain all the values
+ * Return: return the number of nodes
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_nodes;
+
+	if (!(head && n))
+		return (NULL);
+
+	new_nodes = malloc(sizeof(dlistint_t));
+
+	if (new_nodes == NULL)
+		return (NULL);
+
+	new_nodes->n = n;
+	new_nodes->prev = NULL;
+	new_nodes->next = *head;
+	if (*head)
+		(*head)->prev = new_nodes;
+	*head = new_nodes;
+
+	return (new_nodes);
+}
