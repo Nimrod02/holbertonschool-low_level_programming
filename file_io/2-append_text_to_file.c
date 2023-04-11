746ac5bf9b6e7064fe2d909f8fd4a3eeed192519
nimrod02 <letrun.a@gmail.com>
2023-04-11 21:02:16 +0200

    fix return condition of file name

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int openfile, writefile;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
