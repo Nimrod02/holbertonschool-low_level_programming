152c1cef31142788e97cda668ff87ccd60073269
nimrod02 <letrun.a@gmail.com>
2023-04-11 21:01:06 +0200

    fix file for task 2 with betty syntax

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * 
- * 
- * 
- * 
+ * append_text_to_file - main function that add at the end of the text
+ * @filename: pointer to the name of file
+ * @text_content: content of the text in the file
+ *
+ * Return: success
 */
 
 int append_text_to_file(const char *filename, char *text_content)
@@ -17,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	openfile = open(filename, O_APPEND| O_RDWR, 0600);
+	openfile = open(filename, O_APPEND | O_RDWR, 0600);
 
 	writefile = write(openfile, text_content, strlen(text_content));
 
