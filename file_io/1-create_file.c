5417349e04738a3131dd575dc505b2f092ba9e6b
nimrod02 <letrun.a@gmail.com>
2023-04-11 18:31:00 +0200

    add where filename is the name of the file to create and text_content is a NULL terminated string to write to the file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,6 +13,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (0);
 
+	if (text_content == NULL)
+		text_content = "";
+
 	openfile = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 
 	writefile = write(openfile, text_content, strlen(text_content));
