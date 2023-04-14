9ce4702920ca1e97fdfef59f791f776a7aa5f597
nimrod02 <letrun.a@gmail.com>
2023-04-14 20:47:45 +0200

    add file for task 3 cp and update header file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -54,9 +54,10 @@ int main(int argc, char *argv[])
 	char *buffer;
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-
+	}
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
@@ -64,17 +65,20 @@ int main(int argc, char *argv[])
 
 	do {
 		if (from == -1 || r == -1)
+		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
-
+		}
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
+		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
+		}
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
 
