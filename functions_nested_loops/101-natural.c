740282a3863c5470975708bfd8e2f2b5aefd4b9d
nimrod02 <letrun.a@gmail.com>
2023-05-26 09:13:40 +0200

    fix task 101

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -2,15 +2,23 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * main - print the result of multiple of 3 and 5 to 1024
+ *
+ *
+ * Return: always success
+*/
 int main(void)
 {
 	int index, sum = 0;
 
 	for (index = 0; index < 1024; index++)
 	{
-		if (index % 3 == 0|| index % 5 == 0)
+		if (index % 3 == 0 || index % 5 == 0)
+		{
 			sum += index;
 			printf("%d\n", sum);
+		}
 	}
 	return (EXIT_SUCCESS);
 }
